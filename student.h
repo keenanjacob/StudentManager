c440cb1d472e07a549971110b1e069cff75260eb
keenanjacob <kjacob1@mail.csuchico.edu>
2018-04-30 17:22:56 -0700

    first commit

---
+++ student.h
@@ -0,0 +1,17 @@
+#ifndef _STUDENT_H_
+#define _STUDENT_H_
+
+class student
+{
+  private:
+
+  string first;
+  string last;
+
+  public:
+
+  void setName(string first, string last)
+  void fullName(string first, string last)
+};
+
+#endif
